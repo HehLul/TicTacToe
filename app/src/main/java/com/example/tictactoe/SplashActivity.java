2cc077ff737dffb7fe1daa29f0856947fc6a2112
Ahmed Salman <amdsalman04@gmail.com>
2023-07-09 14:56:05 -0700

    Added animation to textview in splash screen

--- app/src/main/java/com/example/tictactoe/SplashActivity.java
+++ app/src/main/java/com/example/tictactoe/SplashActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
+//VARIABLE
+TextView splashText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+            //ANIMATION OF TEXT
+            splashText = findViewById(R.id.splashText);//text id
+            Animation move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);//load move file animation
+            splashText.startAnimation(move);//execute animation, can use startAnimation
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,7 +35,7 @@ public class SplashActivity extends AppCompatActivity {
 
                 finish(); //POP FROM STACK, CANT GO BACK
             }
-        }, 3000);//3 SECOND DELAY
+        }, 4000);//5 SECOND DELAY
 
     }
 }
